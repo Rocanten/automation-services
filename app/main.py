02404fb344ee0468b57c28afae24ccb829676249
Oleg Prokofev <prokofev.o.a@gmail.com>
2022-05-03 14:51:41 +0300

    Added logging to file

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
+import logging
+
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+logging.basicConfig(filename='app.log', encoding='utf-8', level=logging.DEBUG)
 
 app = FastAPI()
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    response = await call_next(request)
+    json = await request.json()
+    print(json)
+    logging.debug(json)
+    return response
 
 @app.get("/")
 def get_main():
     return {
-        "message": "Hello world",
-        "user_message": "CD works!"
+        "message": "Hello world :)",
+        "user_message": "CD works"
     }
 
-@app.post("/api/tracker/time")
+@app.post("/api/timelogged")
 def get_week_time(command: str, ):
     return {
         "response_type": "in_channel",
