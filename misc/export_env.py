bb2a87e594e6884057f577144de6da965ac67021
Oleg Prokofev <prokofev.o.a@gmail.com>
2022-05-03 23:22:15 +0300

    Added vars to actions

--- misc/export_env.py
+++ misc/export_env.py
@@ -1,11 +1,14 @@
 import os
 
 def set_env():
-    with open('.env', 'r') as reader:
-        lines = reader.readlines()
-        for line in lines:
-            text = line.rstrip('\n')
-            name = text.split('=')[0]
-            value = text.split('=')[1]
-            os.environ[name] = value
+    try:
+        with open('.env', 'r') as reader:
+            lines = reader.readlines()
+            for line in lines:
+                text = line.rstrip('\n')
+                name = text.split('=')[0]
+                value = text.split('=')[1]
+                os.environ[name] = value
+    except:
+        return
 
