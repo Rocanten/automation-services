5435834af02eed5cbab24c1e38bf49d3defb2e6d
Oleg Prokofev <prokofev.o.a@gmail.com>
2022-05-04 19:19:04 +0300

    Finished week logic

--- app/models/timelog.py
+++ app/models/timelog.py
@@ -1,4 +1,4 @@
-import isodate
+import isodate, re
 from datetime import datetime, timedelta
 
 class Timelog:
@@ -10,7 +10,24 @@ class Timelog:
         self.author_id = author_id
         self.created = isodate.parse_datetime(created)
         self.start = isodate.parse_datetime(start)
-        self.duration = isodate.parse_duration(duration)
+        weeks = 0
+        weeks_list = re.findall(r"(\d+)W", duration)
+        if weeks_list:
+            weeks = int(weeks_list[0])
+        days = 0
+        days_list = re.findall(r"(\d+)D", duration)
+        if days_list:
+            days = int(days_list[0])
+        minutes = 0
+        minutes_list = re.findall(r"(\d+)M", duration)
+        if minutes_list:
+            minutes = int(minutes_list[0])
+        hours = 0
+        hours_list = re.findall(r"(\d+)H", duration)
+        if hours_list:
+            hours = int(hours_list[0])
+        self.total_seconds = weeks*7*8*60*60 + days*8*60*60 + hours*60*60 + minutes*60
+        print(f'total seconds: {self.total_seconds}')
 
     def __str__(self):
         return ('Timelog:\n' +
@@ -19,8 +36,7 @@ class Timelog:
                 f'Duration: {self.get_formated_duration()}')
 
     def get_formated_duration(self):
-        total_seconds = self.duration.total_seconds()
-        hours = total_seconds%(60*60)
-        minutes = total_seconds%60 - hours*60
-        seconds = total_seconds - hours*60*60
+        hours = self.total_seconds % 60*60
+        minutes = self.total_seconds%60 - hours*60
+        seconds = self.total_seconds - hours*60*60
         return f'Total(hours: {hours}, minutes: {minutes})'
--- app/timelog.py
+++ app/timelog.py
@@ -14,7 +14,7 @@ def get_logged_time(email: str, period: str) -> str:
         timelogs = get_logged_time_period(email, week_start, week_end)
         total_seconds = 0
         for timelog in timelogs:
-            total_seconds += timelog.duration.total_seconds()
+            total_seconds += timelog.total_seconds
         print(total_seconds)
         hours: int = int(total_seconds//(60*60))
         minutes: int = int(total_seconds//60 - hours*60)
@@ -31,7 +31,7 @@ def get_logged_time(email: str, period: str) -> str:
                day_date = day_date.replace(tzinfo=pytz.UTC)
                timelog.start = timelog.start.replace(tzinfo=pytz.UTC)
                if day_date + timedelta(days=1) >= timelog.start > day_date:
-                   day.add_time(timelog.project, timelog.duration.total_seconds())
+                   day.add_time(timelog.project, timelog.total_seconds)
            days_text += str(day)
         
         result = f'{days_text} Total(hours: {hours}, minutes: {minutes})'
